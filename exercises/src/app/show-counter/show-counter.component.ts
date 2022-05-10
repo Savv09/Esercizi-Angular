ce64fae095abef1b337bd7f46ba782e5347e9dc1
Savio Centola <saviocentola@gmail.com>
2022-05-10 13:51:27 +0200

    accantonato

--- exercises/src/app/show-counter/show-counter.component.ts
+++ exercises/src/app/show-counter/show-counter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { CounterService } from '../services/counter.service';
 
 @Component({
@@ -7,10 +8,12 @@ import { CounterService } from '../services/counter.service';
   styleUrls: ['./show-counter.component.css']
 })
 export class ShowCounterComponent implements OnInit {
+  counter$ = new Observable<number>();
 
   constructor(public counterServices: CounterService) { }
 
   ngOnInit(): void {
+    
   }
 
   addValue(value: number) {
@@ -20,4 +23,6 @@ export class ShowCounterComponent implements OnInit {
   subValue(value: number) {
     return this.counterServices.decreaseCounter(value)
   }
+
+
 }
